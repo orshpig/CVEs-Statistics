e3f30aea97d30dc876812caf9c49845bde73789d
orshpig <118303931+orshpig@users.noreply.github.com>
2023-01-04 12:54:51 +0200

    Update plots.py

--- plots.py
+++ plots.py
@@ -3,9 +3,13 @@ import pandas as pd
 
 def to_graph():
     cve = pd.read_csv('cve.csv')
-    plt.bar(cve["date"], cve["total"])
-    plt.xticks(rotation=45)
+    date = cve['date']
+    indexes = [index for index, date in enumerate(date)]
+    dates = [date for index, date in enumerate(date)]
+    dates = list(map(str, dates))
+    plt.bar(dates, cve['total'])
+    plt.xticks(ticks=indexes, labels=dates, rotation=45)
     plt.title('Reported CVEs')
-    plt.xlabel('Date')
+    plt.xlabel('Dates')
     plt.ylabel('Number Of CVEs')
     plt.show()
