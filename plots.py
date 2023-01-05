2abec832b060dc8679e061bab5fcf8233872107b
orshpig <118303931+orshpig@users.noreply.github.com>
2023-01-05 11:11:33 +0200

    Update plots.py

--- plots.py
+++ plots.py
@@ -8,12 +8,12 @@ def to_graph():
     indexes = [index for index, date in enumerate(date)]
     dates = [date for index, date in enumerate(date)]
     dates = list(map(str, dates))
-    fig, ax = plt.subplots(figsize=(12, 8))
+    fig, ax = plt.subplots(figsize=(10, 12))
     plt.bar(dates, total)
     plt.xticks(ticks=indexes, labels=dates, rotation=25)
     plt.title('Reported CVEs')
     plt.xlabel('Dates')
     plt.ylabel('Number Of CVEs')
     for i in range(len(dates)):
-        ax.text(dates[i], total[i], total[i], size=12)
+        ax.text(dates[i], total[i], total[i], size=10, horizontalalignment='center', verticalalignment='top')
     plt.show()
